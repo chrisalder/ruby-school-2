995df9dba37167e5d45bb8a744b012a0dbc7090b
Christian <ing.christian.alderete@gmail.com>
2017-06-20 19:07:55 -0600

    first commit

---
+++ arrays.rb
@@ -0,0 +1,20 @@
+puts 'Crea un arreglo e imprime, después agrega un elemento y vuelve a imprimir,
+      despues imprime el ultimo elemento de tu arreglo.'
+
+# Declara tu primer arreglo aqui
+array = [:hola,"mundo",1]
+
+
+puts "Arreglo original"
+puts array
+
+# Agrega un dato a tu arreglo
+
+array[3] = "hola de nuevo"
+
+puts "Arreglo con nuevo elemento"
+puts array
+
+puts "Ultimo elemento del arreglo"
+puts array.last
+# ruby-school-2
